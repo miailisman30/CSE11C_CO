e7acd162d9a1adc4c90b4484d40ccbea2224e5d9
Victor Macarie <victoryvictor11029@gmail.com>
2026-09-09 16:20:42 +0200

    decoder finished - quick update 2026-09-09 16:20

--- A3/decoder-vic.s
+++ A3/decoder-vic.s
@@ -1,6 +1,12 @@
+.section .bss
+.global DECODED
+DECODED:
+    .space 2048
+
+
 .text
 
-.include "helloWorld.s"
+.include "final.s"
 
 .global main
 
@@ -21,6 +27,45 @@ decode:
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
 
 	# your code goes here
+
+    movq $DECODED, %r8  # set DECODED index to 0
+    movq %rdi, %rsi     # current address is rsi
+    m_block:            # do this for each block
+
+        movl 2(%rsi), %edx # next (as offset from original address)
+        movb 1(%rsi), %ch  # times to print
+        movb 0(%rsi), %cl  # char to print
+
+        print_l: # print ch times
+            cmpb $0, %ch
+            jle e_print_l
+
+            # append to buffer
+            movb %cl, (%r8) # use parenthesis so you dont move into register but at the address the register holds!!!!
+            incq %r8
+
+            decb %ch        # decrement loop
+            
+            jmp print_l
+        e_print_l:
+        
+        # exit loop if arrived on block 0 again
+        cmpl $0, %edx  
+        je e_m_block
+
+        # go to next block
+        shlq $3, %rdx   # multiply next by 8
+        addq %rdi, %rdx # add the original address
+        movq %rdx, %rsi # assign to current block indexer
+        
+        jmp m_block
+    e_m_block:
+    movb $0, (%r8) # ensure termination char
+
+    # print the decoded string
+    movq $DECODED, %rdi
+    movq $0, %rax
+    call printf
 
 	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
